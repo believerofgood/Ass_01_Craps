6995c213d702290701149c6cef95ec9ae8c7ad31
believerofgood <ashurosa@mail.uc.edu>
2025-10-21 04:28:25 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        String playAgain = "Y";
+
+        do {
+            int die1 = rand.nextInt(6) + 1;
+            int die2 = rand.nextInt(6) + 1;
+            int sum = die1 + die2;
+
+            System.out.println("You rolled: " + die1 + " + " + die2 + " = " + sum);
+
+            if (sum == 2 || sum == 3 || sum == 12) {
+                System.out.println("Craps! You crapped out. You lose.");
+            } else if (sum == 7 || sum == 11) {
+                System.out.println("Natural! You win.");
+            } else {
+                int point = sum;
+                System.out.println("Point is set to: " + point);
+                boolean rolling = true;
+
+                while (rolling) {
+                    die1 = rand.nextInt(6) + 1;
+                    die2 = rand.nextInt(6) + 1;
+                    sum = die1 + die2;
+
+                    System.out.println("You rolled: " + die1 + " + " + die2 + " = " + sum);
+
+                    if (sum == point) {
+                        System.out.println("Made point! You win.");
+                        rolling = false;
+                    } else if (sum == 7) {
+                        System.out.println("Rolled a 7. You lose.");
+                        rolling = false;
+                    } else {
+                        System.out.println("Trying for point...");
+                    }
+                }
+            }
+
+            System.out.println("Would you like to play again? [Y/N]");
+            playAgain = in.nextLine();
+
+        } while (playAgain.equalsIgnoreCase("Y"));
+
+        System.out.println("Thanks for playing!");
+    }
+}
+
